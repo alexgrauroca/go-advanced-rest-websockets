a270a0711f483dfe1710c837eb470c3f89e6c9b1
Àlex Grau Roca <alexgrauroca91@gmail.com>
2022-05-27 23:43:05 +0200

    Logging all requests

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"go-advanced-rest-websockets/helpers"
 	"go-advanced-rest-websockets/server"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -17,6 +18,8 @@ var (
 func CheckAuthMiddleware(s server.Server) func(h http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("New request: %+v", r)
+
 			if !shouldCheckToken(r.URL.Path) {
 				next.ServeHTTP(w, r)
 				return
